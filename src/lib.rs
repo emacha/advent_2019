7e88f45595b0fef33d24d6791a2b0aaa909dd4be
emacha <edmacha93@gmail.com>
2019-12-01 12:34:54 +0000

    Add tests for first puzzle.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
+
+pub fn fuel_cost(mass: u64) -> u64 {
+    mass
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
+    use crate::fuel_cost;
+    // Test 4 examples of fuel_cost
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_fuel_cost() {
+        assert_eq!(fuel_cost(12), 2);
+        assert_eq!(fuel_cost(14), 2);
+        assert_eq!(fuel_cost(1969), 654);
+        assert_eq!(fuel_cost(100756), 33583);
     }
 }
