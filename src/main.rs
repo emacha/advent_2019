a1958e25a95c6eb0666175d9240b757066fd1d9d
emacha <edmacha93@gmail.com>
2019-12-01 13:05:19 +0000

    Add solution to first puzzle.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use advent_2019::fuel_cost;
+use std::io::{BufReader, Result, BufRead};
+use std::fs::File;
 
-fn main() {
-    println!("I'm running!")
+
+fn main() -> Result<()> {
+    let input = File::open("inputs/day_1.txt")?;
+    let input = BufReader::new(input);
+
+    let mut fuel_sum = 0;
+    for module_mass in input.lines() {
+        let value: u64 = module_mass.unwrap().parse().unwrap();
+        fuel_sum += fuel_cost(value)
+    }
+
+    println!("{}", fuel_sum);
+    Ok(())
 }
