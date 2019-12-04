fd2f7983736c463734f8b8bacadf1f44e82d577b
emacha <edmacha93@gmail.com>
2019-12-04 00:05:11 +0000

    Alter main to give solution for 1st puzzle.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use advent_2019::*;
-use std::io::Result;
+use std::io::{Result, BufReader, BufRead};
 use std::fs;
 
 #[derive(Debug)]
@@ -10,26 +10,15 @@ struct Memory {
 
 
 fn main() -> Result<()> {
-    let code: String = fs::read_to_string("inputs/day_2.txt").unwrap().trim().parse().unwrap();
+    let input = fs::File::open("inputs/day_3.txt")?;
+    let input = BufReader::new(input);
+    let mut args: Vec<String> = vec![];
+    for line in input.lines() {
+        args.push(line.unwrap());
+        //if let Ok(x) = line.unwrap() {args.push(x)};
+    }
 
-    let search = || {
-        for noun in 0..=99 {
-            for verb in 0..=99 {
-                let new_code = intcode(&new_intcode(&noun.to_string(),
-                                                   &verb.to_string(),
-                                                   &code));
-
-                if &new_code[0..8] == "19690720" {
-                    return Memory {noun, verb};
-                }
-
-            }
-        }
-        Memory {noun: -1, verb: -1}
-    };
-
-    let new_code = search();
     
-    println!("{:?}", new_code);
+    println!("{:?}", intersection_distance(&args[0], &args[1]));
     Ok(())
 }
