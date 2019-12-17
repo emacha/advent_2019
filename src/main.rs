47300cb3b5dd58aac882004ae45f185815a6fd67
emacha <edmacha93@gmail.com>
2019-12-17 16:20:59 -0300

    Write main to check number of possible passwords. ans == 1610

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,9 @@ use advent_2019::*;
 use std::io::{Result, BufReader, BufRead};
 use std::fs;
 
-#[derive(Debug)]
-struct Memory {
-    noun: i32,
-    verb: i32,
-}
-
-
 fn main() -> Result<()> {
-    let input = fs::File::open("inputs/day_3.txt")?;
-    let input = BufReader::new(input);
-    let mut args: Vec<String> = vec![];
-    for line in input.lines() {
-        args.push(line.unwrap());
-        //if let Ok(x) = line.unwrap() {args.push(x)};
-    }
+    let total = (183565..657474).fold(0, |acc, x| acc + (passes_rules(&x)) as i32);
 
-    
-    println!("{:?}", intersection_distance(&args[0], &args[1]));
+    println!("{:?}", total);
     Ok(())
 }
